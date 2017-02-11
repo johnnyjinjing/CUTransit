b3717e605c6e4def2da519c616eecd42f9a3af9f
Jing Jin <johnnyjinjing@gmail.com>
2017-02-11 14:55:30 -0800

    build initial app

---
+++ app/src/main/java/com/example/cutransit/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cutransit.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cutransit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
