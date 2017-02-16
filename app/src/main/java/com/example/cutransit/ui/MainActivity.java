cf7ff33600bac0e3254c4f9ebffd426ae5deb61e
Jing Jin <johnnyjinjing@gmail.com>
2017-02-16 16:13:14 -0600

    implement tabs

--- app/src/main/java/com/example/cutransit/ui/MainActivity.java
+++ app/src/main/java/com/example/cutransit/ui/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.cutransit.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.cutransit.R;
 import com.example.cutransit.util.DataUtils;
 
-public class MainActivity extends AppCompatActivity implements AllStopsFragment.Callback{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AllStopsFragment.Callback {
+
+    private ViewPager mPager;
+    FragmentAdapter mAdapter;
+
 
     static final String INTENT_EXTRA_STOP_ID = "id";
     static final String INTENT_EXTRA_STOP_NAME = "name";
@@ -17,6 +29,18 @@ public class MainActivity extends AppCompatActivity implements AllStopsFragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+
+        mAdapter = new FragmentAdapter(getSupportFragmentManager());
+        mAdapter.addFragment(new AllStopsFragment(), "All Stops");
+        mAdapter.addFragment(new AllStopsFragment(), "Favorites");
+        mAdapter.addFragment(new AllStopsFragment(), "Nearby");
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(mPager);
     }
 
     public void fetchStopsData(View view) {
@@ -30,5 +54,35 @@ public class MainActivity extends AppCompatActivity implements AllStopsFragment.
                 .putExtra(INTENT_EXTRA_STOP_ID, id)
                 .putExtra(INTENT_EXTRA_STOP_NAME, name);
         startActivity(intent);
+    }
+
+    public static class FragmentAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
